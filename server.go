3272e7ebd7786913e0620cb56c74c44fe66c6d50
Константин Мокроусов <konstantinmokrousov@MacBook-Air-Konstantin.local>
2024-10-12 21:57:28 +0700

    first commit

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/hello-world", handleHelloworld)
+	http.HandleFunc("/health", handleHealth)
+	addr := "localhost:8000"
+	log.Printf("Lostening on %s ...", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func handleHelloworld(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+	write(writer, "Hello from func")
+
+}
+func handleHealth(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+
+}
+func write(writer http.ResponseWriter, msg string) {
+	response := []byte(msg)
+	fmt.Println(response)
+
+	_, err := writer.Write(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
